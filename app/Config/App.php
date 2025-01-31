c7dd897d033a8c36d80ae70e59e848d9122cad78
Nadhief Fawaz Rezaka <72719539+nadhfrr@users.noreply.github.com>
2025-01-31 23:44:24 +0700

    edit url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/bis/';
+    public string $baseURL = 'http://localhost/github/ptbis/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
