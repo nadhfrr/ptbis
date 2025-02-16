176a250414310bf2d2cae1de470f2f425b72b4ad
Nadhief Fawaz Rezaka <72719539+nadhfrr@users.noreply.github.com>
2025-02-16 13:46:19 +0700

    up9

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <title>PT Berkah Intan Sejahtera</title>
+    <!-- <link rel="shortcut icon" href="<?= base_url() ?>public/template/assets/images/logo.png"> -->
 
     <!-- Bootstrap core CSS -->
     <link href="<?= base_url() ?>public/template/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="<?= base_url() ?>public/template/assets/css/fontawesome.css">
